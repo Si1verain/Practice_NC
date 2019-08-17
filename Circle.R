5f06920b1fb49aaaa2d4fc676cbae62de9c28700
Si1verain <54216244+Si1verain@users.noreply.github.com>
2019-08-17 14:38:03 -0400

    This is my circle

---
+++ Circle.R
@@ -0,0 +1,15 @@
+#circle
+data <- seq(0,2*pi,length.out = 100)
+x<- cos(data)
+y<- sin(data)
+plot (x,y)
+x <- seq(-1,1,length.out = 1000)
+
+#throw n darts
+n <- 100000
+dx <- runif(n,-1,1)
+dy <- runif(n,-1,1)
+ix <- which(dx^2 + dy^2 <= 1 )
+points(dx[ix], dy [ix], col='green', pch=20)
+points(dx[-ix], dy [-ix], 'orange', pch=20)
+4*length(ix)/n
